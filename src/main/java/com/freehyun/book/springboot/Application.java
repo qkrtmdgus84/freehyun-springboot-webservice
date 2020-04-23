ce4926f2f17a1ebf8af84d4bc95259d265c1b856
84sh <84sh@naver.com>
2020-04-23 11:16:13 +0900

    시큐리티 적용하기

--- src/main/java/com/freehyun/book/springboot/Application.java
+++ src/main/java/com/freehyun/book/springboot/Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing //따로분리 config/JpaConfig 선언
+//@EnableJpaAuditing //따로분리 config/JpaConfig 선언
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
